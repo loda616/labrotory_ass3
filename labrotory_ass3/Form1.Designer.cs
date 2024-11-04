6a5b53a85e664d2f8d417c4df9386876f9c6db1e
Khalid Omr <khalidomr805@gmail.com>
2024-11-04 20:06:57 +0200

    add logic for ui

--- labrotory_ass3/Form1.Designer.cs
+++ labrotory_ass3/Form1.Designer.cs
@@ -28,41 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.firstNameTextBox = new System.Windows.Forms.TextBox();
+            this.lastNameTextBox = new System.Windows.Forms.TextBox();
+            this.studentIDTextBox = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
-            this.button1 = new System.Windows.Forms.Button();
-            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
-            this.button2 = new System.Windows.Forms.Button();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.maleRadioButton = new System.Windows.Forms.RadioButton();
+            this.femaleRadioButton = new System.Windows.Forms.RadioButton();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.subjectsCheckedListBox = new System.Windows.Forms.CheckedListBox();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.showDataTextBox = new System.Windows.Forms.TextBox();
+            this.gradeComboBox = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
-            // textBox1
+            // firstNameTextBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 12);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 22);
-            this.textBox1.TabIndex = 0;
+            this.firstNameTextBox.Location = new System.Drawing.Point(12, 12);
+            this.firstNameTextBox.Name = "firstNameTextBox";
+            this.firstNameTextBox.Size = new System.Drawing.Size(100, 22);
+            this.firstNameTextBox.TabIndex = 0;
+            this.firstNameTextBox.TextChanged += new System.EventHandler(this.firstNameTextBox_TextChanged);
             // 
-            // textBox2
+            // lastNameTextBox
             // 
-            this.textBox2.Location = new System.Drawing.Point(12, 40);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 22);
-            this.textBox2.TabIndex = 1;
+            this.lastNameTextBox.Location = new System.Drawing.Point(12, 40);
+            this.lastNameTextBox.Name = "lastNameTextBox";
+            this.lastNameTextBox.Size = new System.Drawing.Size(100, 22);
+            this.lastNameTextBox.TabIndex = 1;
+            this.lastNameTextBox.TextChanged += new System.EventHandler(this.lastNameTextBox_TextChanged);
             // 
-            // textBox3
+            // studentIDTextBox
             // 
-            this.textBox3.Location = new System.Drawing.Point(12, 68);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 22);
-            this.textBox3.TabIndex = 2;
+            this.studentIDTextBox.Location = new System.Drawing.Point(12, 68);
+            this.studentIDTextBox.Name = "studentIDTextBox";
+            this.studentIDTextBox.Size = new System.Drawing.Size(100, 22);
+            this.studentIDTextBox.TabIndex = 2;
+            this.studentIDTextBox.TextChanged += new System.EventHandler(this.studentIDTextBox_TextChanged);
             // 
             // label1
             // 
@@ -91,95 +94,106 @@
             this.label3.TabIndex = 5;
             this.label3.Text = "Student ID";
             // 
-            // radioButton1
+            // maleRadioButton
             // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(239, 12);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(58, 20);
-            this.radioButton1.TabIndex = 6;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "Male";
-            this.radioButton1.UseVisualStyleBackColor = true;
+            this.maleRadioButton.AutoSize = true;
+            this.maleRadioButton.Location = new System.Drawing.Point(239, 12);
+            this.maleRadioButton.Name = "maleRadioButton";
+            this.maleRadioButton.Size = new System.Drawing.Size(58, 20);
+            this.maleRadioButton.TabIndex = 6;
+            this.maleRadioButton.TabStop = true;
+            this.maleRadioButton.Text = "Male";
+            this.maleRadioButton.UseVisualStyleBackColor = true;
+            this.maleRadioButton.CheckedChanged += new System.EventHandler(this.maleRadioButton_CheckedChanged);
             // 
-            // radioButton2
+            // femaleRadioButton
             // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(239, 40);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(74, 20);
-            this.radioButton2.TabIndex = 7;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "Female";
-            this.radioButton2.UseVisualStyleBackColor = true;
-            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            this.femaleRadioButton.AutoSize = true;
+            this.femaleRadioButton.Location = new System.Drawing.Point(239, 40);
+            this.femaleRadioButton.Name = "femaleRadioButton";
+            this.femaleRadioButton.Size = new System.Drawing.Size(74, 20);
+            this.femaleRadioButton.TabIndex = 7;
+            this.femaleRadioButton.TabStop = true;
+            this.femaleRadioButton.Text = "Female";
+            this.femaleRadioButton.UseVisualStyleBackColor = true;
+            this.femaleRadioButton.CheckedChanged += new System.EventHandler(this.femaleRadioButton_CheckedChanged);
             // 
-            // button1
+            // clearButton
             // 
-            this.button1.Location = new System.Drawing.Point(240, 299);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(113, 47);
-            this.button1.TabIndex = 8;
-            this.button1.Text = "Clear";
-            this.button1.UseVisualStyleBackColor = true;
+            this.clearButton.Location = new System.Drawing.Point(240, 299);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(113, 47);
+            this.clearButton.TabIndex = 8;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
             // 
-            // checkedListBox1
+            // subjectsCheckedListBox
             // 
-            this.checkedListBox1.FormattingEnabled = true;
-            this.checkedListBox1.Items.AddRange(new object[] {
+            this.subjectsCheckedListBox.FormattingEnabled = true;
+            this.subjectsCheckedListBox.Items.AddRange(new object[] {
             "Math",
             "Science",
             "Literature"});
-            this.checkedListBox1.Location = new System.Drawing.Point(347, 12);
-            this.checkedListBox1.Name = "checkedListBox1";
-            this.checkedListBox1.Size = new System.Drawing.Size(120, 89);
-            this.checkedListBox1.TabIndex = 9;
+            this.subjectsCheckedListBox.Location = new System.Drawing.Point(347, 12);
+            this.subjectsCheckedListBox.Name = "subjectsCheckedListBox";
+            this.subjectsCheckedListBox.Size = new System.Drawing.Size(120, 89);
+            this.subjectsCheckedListBox.TabIndex = 9;
+            this.subjectsCheckedListBox.SelectedIndexChanged += new System.EventHandler(this.subjectsCheckedListBox_SelectedIndexChanged);
             // 
-            // button2
+            // submitButton
             // 
-            this.button2.Location = new System.Drawing.Point(121, 299);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(113, 47);
-            this.button2.TabIndex = 10;
-            this.button2.Text = "Submit";
-            this.button2.UseVisualStyleBackColor = true;
+            this.submitButton.Location = new System.Drawing.Point(121, 299);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(113, 47);
+            this.submitButton.TabIndex = 10;
+            this.submitButton.Text = "Submit";
+            this.submitButton.UseVisualStyleBackColor = true;
+            this.submitButton.Click += new System.EventHandler(this.submitButton_Click);
             // 
-            // textBox4
+            // showDataTextBox
             // 
-            this.textBox4.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.textBox4.Location = new System.Drawing.Point(12, 141);
-            this.textBox4.Multiline = true;
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(135, 92);
-            this.textBox4.TabIndex = 11;
-            this.textBox4.Text = "First Name :\r\nLast Nmae :\r\nStudent ID : \r\nGender : \r\nSubject : ";
+            this.showDataTextBox.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.showDataTextBox.Location = new System.Drawing.Point(12, 141);
+            this.showDataTextBox.Multiline = true;
+            this.showDataTextBox.Name = "showDataTextBox";
+            this.showDataTextBox.Size = new System.Drawing.Size(273, 92);
+            this.showDataTextBox.TabIndex = 11;
+            this.showDataTextBox.Text = "First Name :\r\nLast Nmae :\r\nStudent ID : \r\nGender : \r\nSubject : ";
+            this.showDataTextBox.TextChanged += new System.EventHandler(this.showDataTextBox_TextChanged);
             // 
-            // comboBox1
+            // gradeComboBox
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(473, 12);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 24);
-            this.comboBox1.TabIndex = 12;
+            this.gradeComboBox.FormattingEnabled = true;
+            this.gradeComboBox.Items.AddRange(new object[] {
+            "Freshman",
+            "Sophomore",
+            "Junior",
+            "Senior"});
+            this.gradeComboBox.Location = new System.Drawing.Point(473, 12);
+            this.gradeComboBox.Name = "gradeComboBox";
+            this.gradeComboBox.Size = new System.Drawing.Size(121, 24);
+            this.gradeComboBox.TabIndex = 12;
+            this.gradeComboBox.SelectedIndexChanged += new System.EventHandler(this.gradeComboBox_SelectedIndexChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.checkedListBox1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.radioButton2);
-            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.gradeComboBox);
+            this.Controls.Add(this.showDataTextBox);
+            this.Controls.Add(this.submitButton);
+            this.Controls.Add(this.subjectsCheckedListBox);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.femaleRadioButton);
+            this.Controls.Add(this.maleRadioButton);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.studentIDTextBox);
+            this.Controls.Add(this.lastNameTextBox);
+            this.Controls.Add(this.firstNameTextBox);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -189,19 +203,19 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox firstNameTextBox;
+        private System.Windows.Forms.TextBox lastNameTextBox;
+        private System.Windows.Forms.TextBox studentIDTextBox;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.RadioButton radioButton1;
-        private System.Windows.Forms.RadioButton radioButton2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.CheckedListBox checkedListBox1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.RadioButton maleRadioButton;
+        private System.Windows.Forms.RadioButton femaleRadioButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.CheckedListBox subjectsCheckedListBox;
+        private System.Windows.Forms.Button submitButton;
+        private System.Windows.Forms.TextBox showDataTextBox;
+        private System.Windows.Forms.ComboBox gradeComboBox;
     }
 }
 
--- labrotory_ass3/Form1.cs
+++ labrotory_ass3/Form1.cs
@@ -17,9 +17,86 @@ namespace labrotory_ass3
             InitializeComponent();
         }
 
-        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        private void maleRadioButton_CheckedChanged(object sender, EventArgs e)
         {
+        }
 
+        private void femaleRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void subjectsCheckedListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void gradeComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void firstNameTextBox_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void lastNameTextBox_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void studentIDTextBox_TextChanged(object sender, EventArgs e)
+        {
+            // Ensure the "Student ID" field accepts only numeric input
+            if (System.Text.RegularExpressions.Regex.IsMatch(studentIDTextBox.Text, @"[^0-9]"))
+            {
+                studentIDTextBox.Text = studentIDTextBox.Text.Remove(studentIDTextBox.Text.Length - 1);
+            }
+        }
+
+        private void showDataTextBox_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void submitButton_Click(object sender, EventArgs e)
+        {
+            // Validate that all required fields are filled out
+            if (string.IsNullOrEmpty(firstNameTextBox.Text) || string.IsNullOrEmpty(lastNameTextBox.Text) || string.IsNullOrEmpty(studentIDTextBox.Text))
+            {
+                MessageBox.Show("Please fill out all required fields.");
+                return;
+            }
+
+            // Validate that at least one interest checkbox is selected
+            bool isAnySubjectSelected = subjectsCheckedListBox.CheckedItems.Count > 0;
+            if (isAnySubjectSelected)
+            {
+                // Display a summary of the entered data
+                string summary = $"Full Name: {firstNameTextBox.Text} {lastNameTextBox.Text}\n";
+                summary += $"Student ID: {studentIDTextBox.Text}\n";
+                summary += $"Gender: {(maleRadioButton.Checked ? "Male" : "Female")}\n";
+                summary += "Interests:\n";
+                foreach (var subject in subjectsCheckedListBox.CheckedItems)
+                {
+                    summary += $"- {subject}\n";
+                }
+                summary += $"Grade Level: {gradeComboBox.SelectedItem}";
+
+                showDataTextBox.Text = summary;
+            }
+            else
+            {
+                MessageBox.Show("Please select at least one area of interest.");
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            // Reset all form fields
+            firstNameTextBox.Text = string.Empty;
+            lastNameTextBox.Text = string.Empty;
+            studentIDTextBox.Text = string.Empty;
+            maleRadioButton.Checked = false;
+            femaleRadioButton.Checked = false;
+            subjectsCheckedListBox.ClearSelected();
+            gradeComboBox.SelectedIndex = -1;
+            showDataTextBox.Text = string.Empty;
         }
     }
 }
